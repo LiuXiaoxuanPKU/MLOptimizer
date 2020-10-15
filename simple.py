1f99955ef79402ab09e62551279a5e69489c55c2
lilyliu <lilyliupku@gmail.com>
2020-10-15 07:13:48 -0700

    fix

--- simple.py
+++ simple.py
@@ -20,22 +20,20 @@ D_out = D_in
 # Create random Tensors to hold inputs and outputs
 x = torch.randn(N, D_in).cuda()
 #y = torch.randn(N, D_out)
-y = 2 * x.mul(3.5)  * x + x + x.sqrt() + 1
+y = 2 * x.mul(3.5)  * x + x + x.absolute().sqrt() + 1
 
 print(x)
 print(y)
 model = torch.nn.Sequential(
-    BlockSparseLinear(N, D_in, True, 1),
+    BlockSparseLinear(D_in, H1, True, 0.1),
     torch.nn.ReLU(),
-    BlockSparseLinear(D_in, H1, True, 1),
+    BlockSparseLinear(H1, H2, True, 0.1),
     torch.nn.ReLU(),
-    BlockSparseLinear(H1, H2, True, 1),
-    torch.nn.ReLU(),
-    BlockSparseLinear(H2, D_out, True, 1)
+    BlockSparseLinear(H2, D_out, True, 0.1)
 )
 
-org_para_num = D_in * H1, H1 * H2, H2 * D_out
-sparse_para_num = model.num_parameters()
+org_para_num = D_in * H1 + H1 * H2 + H2 * D_out
+sparse_para_num = sum(p.numel() for p in model.parameters())
 
 print("Original parameter number =========>", org_para_num)
 print("Parameter Number =====>", sparse_para_num)
@@ -46,7 +44,7 @@ loss_fn = torch.nn.MSELoss(reduction='sum')
 
 learning_rate = 1e-4
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-for t in range(500000):
+for t in range(50000):
     # Forward pass: compute predicted y by passing x to the model.
     gpu_tracker.track()
     y_pred = model(x)
@@ -55,7 +53,7 @@ for t in range(500000):
     loss = loss_fn(y_pred, y)
 
     gpu_tracker.track()
-    if t % 10000 == 99:
+    if t % 1000 == 99:
         print(t, loss.item())
 
     optimizer.zero_grad()
