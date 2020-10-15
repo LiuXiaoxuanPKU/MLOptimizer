ca6392e27bb25a6982789d6075c418cd33e8f3bc
lilyliu <lilyliupku@gmail.com>
2020-10-15 06:23:46 -0700

    simple fit with W sparse

--- simple.py
+++ simple.py
@@ -1,48 +1,39 @@
-from pytorch_block_sparse import BlockSparseLinear
+from unittest import TestCase
 import torch
+import unittest
+import torch.optim as optim
+from pytorch_block_sparse import BlockSparseLinear
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print("Device =========>", device)
+print("Device =====>", device)
 
-N, D_in, H, D_out = 10, 96, 32, 32
+N, D_in, H, D_out = 64, 32, 32, 10
 
-x = torch.randn(N, D_in)
-y = torch.randn(N, D_out)
-
-model = torch.nn.Sequential(
-        BlockSparseLinear(D_in, H, density=0.1),
-        torch.nn.ReLU(),
-        BlockSparseLinear(H, D_out, density=0.1) 
-    )
-
+# Create random Tensors to hold inputs and outputs
+x = torch.randn(N, D_in).cuda()
+#y = torch.randn(N, D_out)
+y = 2 * x
+print(x)
+print(y)
+model = BlockSparseLinear(32, 32, True, 1)
 model = model.to(device)
 
 loss_fn = torch.nn.MSELoss(reduction='sum')
 
 learning_rate = 1e-4
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-for t in range(500):
-    print(i)
-
+for t in range(500000):
     # Forward pass: compute predicted y by passing x to the model.
     y_pred = model(x)
 
     # Compute and print loss.
     loss = loss_fn(y_pred, y)
-    if t % 100 == 99:
+    if t % 10000 == 99:
         print(t, loss.item())
 
-    # Before the backward pass, use the optimizer object to zero all of the
-    # gradients for the variables it will update (which are the learnable
-    # weights of the model). This is because by default, gradients are
-    # accumulated in buffers( i.e, not overwritten) whenever .backward()
-    # is called. Checkout docs of torch.autograd.backward for more details.
     optimizer.zero_grad()
-
-    # Backward pass: compute gradient of the loss with respect to model
-    # parameters
     loss.backward()
-
-    # Calling the step function on an Optimizer makes an update to its
-    # parameters
     optimizer.step()
+
+
+
